5d008d02b80fc9ee67c51d2b46b5cd6bafaea1d3
Ad1tya-007 <adityakul0314@gmail.com>
2025-05-29 12:25:40 -0700

    changed names in .env

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,9 +1,9 @@
 import OpenAI from 'openai'
 
-if (!process.env.OPENAI_API_KEY) {
-  throw new Error('OPENAI_API_KEY is not set')
+if (!process.env.OPENAI_KEY) {
+  throw new Error('OPENAI_KEY is not set')
 }
 
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY!,
+  apiKey: process.env.OPENAI_KEY!,
 })
